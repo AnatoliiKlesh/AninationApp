9e8d0953a9831f61358322c3241262c33e4ee3f5
Anatolii <AJR123123@proton>
2019-10-18 21:39:35 -0700

    Create Animation

--- AninationApp/ViewController.swift
+++ AninationApp/ViewController.swift
@@ -6,15 +6,34 @@
 //  Copyright © 2019 Anatolii Klesh. All rights reserved.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
+    @IBOutlet var animationView: SpringView!
+    @IBOutlet var animationButton: SpringButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        animationView.animate()
+        animationButton.animate()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func startAnimation() {
+        animationView.animation = "pop"
+        animationView.curve = "easeIn"
+        animationView.force = 2
+        animationView.duration = 1
+        animationView.animate()
+        
+        animationButton.animation = "shake"
+        animationButton.curve = "easeOut"
+        animationButton.force = 2
+        animationButton.duration = 2
+        animationButton.animate()
+    }
+    
 
 }
 
